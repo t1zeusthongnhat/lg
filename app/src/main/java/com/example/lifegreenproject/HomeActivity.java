3a928ea67abff9e884bba2d1f2646a25cc2b6676
t1zeusthongnhat <acekem4k2@gmail.com>
2024-10-11 15:04:00 +0700

    update click icon menu

--- app/src/main/java/com/example/lifegreenproject/HomeActivity.java
+++ app/src/main/java/com/example/lifegreenproject/HomeActivity.java
@@ -7,14 +7,42 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.widget.ImageView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity{
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        AppCompatButton learnAndPlayButton = findViewById(R.id.button2bb);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ImageView iconMenu = findViewById(R.id.iconMenu);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        iconMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            int id = menuItem.getItemId();
+            // Handle navigation view item clicks here
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+        AppCompatButton learnAndPlayButton = findViewById(R.id.button2bb);
         learnAndPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,7 +50,5 @@ public class HomeActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-
-
     }
 }
